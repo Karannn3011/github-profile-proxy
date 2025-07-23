6e40e48fb85111a0cc6b9011bd2541166c5bfe24
Karannn3011 <karanchhipa30@gmail.com>
2025-07-24 00:49:41 +0530

    cors correction

--- src/main/java/com/karan/githubproxy/GitHubProxyController.java
+++ src/main/java/com/karan/githubproxy/GitHubProxyController.java
@@ -34,7 +34,7 @@ public class GitHubProxyController {
      * @param username The GitHub username to search for.
      * @return A Mono containing the JSON response from the GitHub API as a String.
      */
-    @CrossOrigin(origins = "http://localhost:5173")
+    @CrossOrigin(origins = {"http://localhost:3000", "http://localhost:5173"})
     @GetMapping("/api/users/{username}")
     public Mono<String> getUserProfile(@PathVariable String username) {
         System.out.println("Proxying request for user profile: " + username);
